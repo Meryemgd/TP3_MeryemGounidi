22e7af6e85efae9f85a759b8a3ed37c4c3fab4dd
Meryemgd <meryemgounidi2@gmail.com>
2025-10-31 21:39:48 +0100

    état initial du projet

---
+++ src/main/java/ma/emsi/gounidimeryem/tp3_gounidimeryem/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.gounidimeryem.tp3_gounidimeryem;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
